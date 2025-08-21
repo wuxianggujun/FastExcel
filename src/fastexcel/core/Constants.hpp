0d16e49ffb07fa1e2fc3914b6b8a3c92451d216a
wuxianggujun <3344207732@qq.com>
2025-08-21 23:20:02 +0800

    优化代码并且删除多余的注释

---
+++ src/fastexcel/core/Constants.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace fastexcel {
+namespace core {
+
+// 通用常量集中定义，便于统一调整与复用
+struct Constants {
+    // I/O 缓冲区大小
+    static constexpr size_t kIOBufferSize = 8192;
+};
+
+} // namespace core
+} // namespace fastexcel
+
